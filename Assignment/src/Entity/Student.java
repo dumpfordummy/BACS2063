0c5fc52f594d276778728d4f6419ad941e8308b2
wailoc25 <frostwl25@gmail.com>
2023-08-26 00:25:15 +0800

    Added entity classes
    added Programme.java, Student.java

---
+++ Assignment/src/Entity/Student.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Entity;
+
+/**
+ *
+ * @author Wai Loc
+ */
+public class Student {
+    private String fullName;
+    private String studID;
+    private String email;
+
+    public Student() {
+    }
+
+    public Student(String fullName, String studID, String email) {
+        this.fullName = fullName;
+        this.studID = studID;
+        this.email = email;
+    }
+    
+}
