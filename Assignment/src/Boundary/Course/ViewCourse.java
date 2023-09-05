f31b46da1ec7fae972a789b4912d95d06e4c4c8f
WymanHoh <85876338+WymanHoh@users.noreply.github.com>
2023-09-05 12:05:43 +0800

    update a bit viewCourse

--- Assignment/src/Boundary/Course/ViewCourse.java
+++ Assignment/src/Boundary/Course/ViewCourse.java
@@ -27,17 +27,29 @@ public class ViewCourse extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        CourseTitle = new javax.swing.JLabel();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        CourseTitle.setFont(new java.awt.Font("Segoe UI", 1, 24)); // NOI18N
+        CourseTitle.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        CourseTitle.setText("View ALL Course");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(181, 181, 181)
+                .addComponent(CourseTitle)
+                .addContainerGap(180, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(27, 27, 27)
+                .addComponent(CourseTitle)
+                .addContainerGap(285, Short.MAX_VALUE))
         );
 
         pack();
@@ -79,5 +91,6 @@ public class ViewCourse extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel CourseTitle;
     // End of variables declaration//GEN-END:variables
 }
